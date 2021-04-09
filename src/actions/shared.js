3760e20a99e376b171ae30460b0cc8774687169d
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2021-04-09 09:55:38 +0100

    created reducers

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,12 +1,16 @@
 import { getInitialData } from "../utils/api";
 import { receiveUsers } from "./users";
 import { receiveQuestions } from "./questions";
+import {showLoading, hideLoading} from 'react-redux-loading'
+
+const AUTHED_ID = 'sarahedo'
 
 export const handleInitialData = () => dispatch => {
     dispatch(showLoading());
     return getInitialData().then(({ users, questions }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveQuestions(questions));
+      dispatch(setAuthedUser(AUTHED_ID))
       dispatch(hideLoading());
     });
   };
