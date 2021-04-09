9e9585e16d991f6cde1272ee8334391068e1a645
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2021-04-09 13:08:45 +0100

    new changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -102,8 +102,6 @@ class Login extends Component {
                 </Message>
                 <div className="field">
                   Select a user from above and click the login button.
-                  <br />
-                  This is a demo app and doesn't require a password.
                 </div>
                 <div
                   className="ui fluid black submit button"
